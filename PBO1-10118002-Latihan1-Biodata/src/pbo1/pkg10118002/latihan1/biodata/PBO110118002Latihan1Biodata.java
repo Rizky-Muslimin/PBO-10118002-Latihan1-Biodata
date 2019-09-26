89166e5cf05e25cab5ec73e6a032e3499ffbf003
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-09-26 15:18:34 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan1-Biodata/src/pbo1/pkg10118002/latihan1/biodata/PBO110118002Latihan1Biodata.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan1.biodata;
+
+/**
+ *
+ * @author ASUS
+ * Nama     : Rizky Muslimin
+ * Kelas    : IF1
+ * NIM      : 10118002
+ * 
+ */
+public class PBO110118002Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "------------------");
+        System.out.println("NAMA\t\t: Rizky Muslimin");
+        System.out.println("Alamat\t\t: Jalan Dago Barat");
+        System.out.println("Tempat Lahir\t: 12 Juli 2000");
+        System.out.println("------------------");
+        System.out.println("HOBI\t\t: Renang & Lari");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+    
+}
